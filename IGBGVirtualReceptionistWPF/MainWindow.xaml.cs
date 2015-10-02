b1a260f88776713a44bd18fb1c91cfea7364a240
mechtateliat <horencho@gmail.com>
2015-10-02 15:58:59 +0300

    Commands, etc.

--- IGBGVirtualReceptionistWPF/MainWindow.xaml.cs
+++ IGBGVirtualReceptionistWPF/MainWindow.xaml.cs
@@ -1,22 +1,12 @@
-﻿using System;
+﻿using IGBGVirtualReceptionist.LyncCommunication;
+using Microsoft.Practices.Prism.Commands;
+using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using IGBGVirtualReceptionist.LyncCommunication;
-using System.Timers;
-using Microsoft.Lync.Model;
+
 
 namespace IGBGVirtualReceptionist
 {
@@ -26,31 +16,20 @@ namespace IGBGVirtualReceptionist
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
         private LyncService lyncService;
+        public static BitmapImage dummyPic = new BitmapImage();
 
         private List<ContactInfo> currentSearchResults = new List<ContactInfo>();
 
         public MainWindow()
         {
             InitializeComponent();
-
             ApplyThemes();
-            Persons = new ObservableCollection<Person>();
-
-            Persons.Add(new Person() { Name = "John Smith", Age = 20 });
-            Persons.Add(new Person() { Name = "Patty Smith", Age = 20 });
-            Persons.Add(new Person() { Name = "John Doe", Age = 20 });
-            Persons.Add(new Person() { Name = "Alan White", Age = 20 });
-            Persons.Add(new Person() { Name = "Ritchie Brown", Age = 20 });
-            Persons.Add(new Person() { Name = "Alex Right", Age = 20 });
-            Persons.Add(new Person() { Name = "Mayeble Own", Age = 20 });
-
-            //     SelectedPersons = new List<Person>();
-            //     SelectedPersons.Add(new Person() { Name = "Mayeble Own", Age = 20 });
-            this.DataContext = this;
+       //     this.DataContext = this;
 
             this.lyncService = new LyncService();
             this.lyncService.ClientStateChanged += this.LyncServiceClientStateChanged;
             this.lyncService.SearchContactsFinished += this.LyncServiceSearchContactsFinished;
+            this.favTiles.ItemsSource = lyncService.GetFavoriteContacts();
         }
 
         private void ApplyThemes()
@@ -63,14 +42,27 @@ namespace IGBGVirtualReceptionist
 
             this.Resources.MergedDictionaries.Add(new ResourceDictionary() { Source = sourceUri });
 
+            dummyPic = new BitmapImage(new Uri(string.Format("pack://application:,,,/{0};component/{1}", assemblyFullName, @"Images\NoPic.bmp")));
+        }
+
+
+        
+        
+        public DelegateCommand<ContactInfo> CallCommand
+        {
+            get
+            {
+                return new DelegateCommand<ContactInfo>(CallAction);
+            }
+        }
+
+        public void CallAction(ContactInfo hh)
+        {
+
         }
 
         private void LyncServiceSearchContactsFinished(object sender, SearchContactsEventArgs e)
         {
-            // TODO: remove this list if not needed
-            //this.currentSearchResults.Clear();
-            //this.currentSearchResults.AddRange(e.FoundContacts);
-
             // populate datasource
             Dispatcher.BeginInvoke((Action)(() =>
             {
@@ -86,46 +78,6 @@ namespace IGBGVirtualReceptionist
                 sbiStatus.Content = e.NewState.ToString();
             }));
 
-        }
-
-        private List<Person> _searchResults;
-        public List<Person> SearchResults
-        {
-            get
-            {
-                return _searchResults;
-            }
-
-            set
-            {
-                _searchResults = value; NotifyPropertyChanged("SearchResults");
-            }
-        }
-        public ObservableCollection<Person> _persons;
-        public ObservableCollection<Person> Persons
-        {
-            get
-            {
-                return _persons;
-            }
-
-            set
-            {
-                _persons = value; NotifyPropertyChanged("Persons");
-            }
-        }
-        public List<Person> _selectedPersons;
-        public List<Person> SelectedPersons
-        {
-            get
-            {
-                return _selectedPersons;
-            }
-
-            set
-            {
-                _selectedPersons = value; NotifyPropertyChanged("SelectedPersons");
-            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -148,30 +100,6 @@ namespace IGBGVirtualReceptionist
         {
             this.lyncService.StartSearchForContactsOrGroups(searchBox.Text);
         }
-
-        //  private void PersonComboEditor_SelectionChanged(object sender, Infragistics.Controls.Editors.SelectionChangedEventArgs e)
-        //  {
-        //      //if (e.AddedItems.Count > 0)
-        //      //    gridPersonDetailsPanel.Visibility = System.Windows.Visibility.Visible;
-        //      //else
-        //      //    gridPersonDetailsPanel.Visibility = System.Windows.Visibility.Collapsed;
-        //      SelectedPersons = new List<Person>();
-        //      SelectedPersons.Add(e.AddedItems[0] as Person);
-
-        ////      xamDataCards.DataSource = SelectedPersons;
-        // }
     }
 
-    public class MyConv : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            return value.ToString();
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
 }
