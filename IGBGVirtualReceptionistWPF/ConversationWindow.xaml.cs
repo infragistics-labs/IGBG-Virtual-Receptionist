360840504c3674d6bf47785b34260ee8ea2e9b27
Borislav Kulov <bkulov@gmail.com>
2015-10-05 14:01:15 +0300

    Implement audio/video event handlers

--- IGBGVirtualReceptionistWPF/ConversationWindow.xaml.cs
+++ IGBGVirtualReceptionistWPF/ConversationWindow.xaml.cs
@@ -46,16 +46,12 @@ namespace IGBGVirtualReceptionist
         protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
         {
             //need to remove event listeners otherwide events may be received after the form has been unloaded
-            //conversation.StateChanged -= conversation_StateChanged;
-            //conversation.ParticipantAdded -= conversation_ParticipantAdded;
-            //conversation.ParticipantRemoved -= conversation_ParticipantRemoved;
-            //conversation.ActionAvailabilityChanged -= conversation_ActionAvailabilityChanged;
-            //avModality.ActionAvailabilityChanged -= avModality_ActionAvailabilityChanged;
-            //avModality.ModalityStateChanged -= avModality_ModalityStateChanged;
-            //audioChannel.ActionAvailabilityChanged -= audioChannel_ActionAvailabilityChanged;
-            //audioChannel.StateChanged -= audioChannel_StateChanged;
-            //videoChannel.ActionAvailabilityChanged -= videoChannel_ActionAvailabilityChanged;
-            //videoChannel.StateChanged -= videoChannel_StateChanged;
+            conversation.StateChanged -= this.ConversationStateChanged;
+            avModality.ActionAvailabilityChanged -= this.AvModalityActionAvailabilityChanged;
+            avModality.ModalityStateChanged -= this.AvModalityModalityStateChanged;
+            audioChannel.StateChanged -= this.AudioChannelStateChanged;
+            videoChannel.StateChanged -= this.VideoChannelStateChanged;
+
             //if the conversation is active, will end it
             if (conversation.State != ConversationState.Terminated)
             {
@@ -86,6 +82,33 @@ namespace IGBGVirtualReceptionist
             base.OnClosing(e);
         }
  
+        private void InitializeConversation()
+        {
+            //saves the AVModality, AudioChannel and VideoChannel, just for the sake of readability
+            avModality = (AVModality)conversation.Modalities[ModalityTypes.AudioVideo];
+            audioChannel = avModality.AudioChannel;
+            videoChannel = avModality.VideoChannel;
+            
+            // TODO: fix the UI
+
+            //show the current conversation and modality states in the UI
+            this.conversationStatus.Text = conversation.State.ToString();
+            this.SetModalityStatus(avModality.State.ToString());
+            this.SetAudioStatus("Disconnected");
+            this.SetVideoStatus("Disconnected");
+
+            //registers for conversation state updates
+            conversation.StateChanged += this.ConversationStateChanged;
+            //subscribes to modality action availability events (all audio button except DTMF)
+            avModality.ActionAvailabilityChanged += this.AvModalityActionAvailabilityChanged;
+            //subscribes to the modality state changes so that the status bar gets updated with the new state
+            avModality.ModalityStateChanged += this.AvModalityModalityStateChanged;
+            //subscribes to the video channel state changes so that the status bar gets updated with the new state
+            audioChannel.StateChanged += this.AudioChannelStateChanged;
+            //subscribes to the video channel state changes so that the video feed can be presented
+            videoChannel.StateChanged += this.VideoChannelStateChanged;
+        }
+ 
         private void InitiateAudioCall()
         {
             //starts an audio call or conference by connecting the AvModality
@@ -113,49 +136,6 @@ namespace IGBGVirtualReceptionist
             }
         }
 
-        private void InitializeConversation()
-        {
-            //saves the AVModality, AudioChannel and VideoChannel, just for the sake of readability
-            avModality = (AVModality)conversation.Modalities[ModalityTypes.AudioVideo];
-            audioChannel = avModality.AudioChannel;
-            videoChannel = avModality.VideoChannel;
-
-            // TODO: fix the UI
-
-            ////show the current conversation and modality states in the UI
-            //toolStripStatusLabelConvesation.Text = conversation.State.ToString();
-            //toolStripStatusLabelModality.Text = avModality.State.ToString();
-
-            ////enables and disables the checkbox associated with the ConversationProperty.AutoTerminateOnIdle property
-            ////based on whether the Lync client is running in InSuppressedMode
-            ////see more details in the checkBoxAutoTerminateOnIdle_CheckStateChanged() method
-            //checkBoxAutoTerminateOnIdle.Enabled = client.InSuppressedMode;
-
-            ////registers for conversation state updates
-            //conversation.StateChanged += conversation_StateChanged;
-
-            ////subscribes to the conversation action availability events (for the ability to add/remove participants)
-            //conversation.ActionAvailabilityChanged += conversation_ActionAvailabilityChanged;
-
-            ////subscribes to modality action availability events (all audio button except DTMF)
-            //avModality.ActionAvailabilityChanged += avModality_ActionAvailabilityChanged;
-
-            ////subscribes to the modality state changes so that the status bar gets updated with the new state
-            //avModality.ModalityStateChanged += avModality_ModalityStateChanged;
-
-            ////subscribes to the audio channel action availability events (DTMF only)
-            //audioChannel.ActionAvailabilityChanged += audioChannel_ActionAvailabilityChanged;
-
-            ////subscribes to the video channel state changes so that the status bar gets updated with the new state
-            //audioChannel.StateChanged += audioChannel_StateChanged;
-
-            ////subscribes to the video channel action availability events
-            //videoChannel.ActionAvailabilityChanged += videoChannel_ActionAvailabilityChanged;
-
-            ////subscribes to the video channel state changes so that the video feed can be presented
-            //videoChannel.StateChanged += videoChannel_StateChanged;
-        }
-
         private void ApplyThemes()
         {
             var assemblyFullName = this.GetType().Assembly.FullName;
@@ -180,5 +160,150 @@ namespace IGBGVirtualReceptionist
                     break;
             }
         }
+
+        private void SetModalityStatus(string status)
+        {
+            Dispatcher.BeginInvoke((Action)(() =>
+            {
+                this.modalityStatus.Text = "Modality: " + status;
+            }));
+        }
+
+        private void SetAudioStatus(string status)
+        {
+            Dispatcher.BeginInvoke((Action)(() =>
+            {
+                this.audioStatus.Text = "Audio: " + status;
+            }));
+        }
+
+        private void SetVideoStatus(string status)
+        {
+            Dispatcher.BeginInvoke((Action)(() =>
+            {
+                this.videoStatus.Text = "Video: " + status;
+            }));
+        }
+
+        private void ConversationStateChanged(object sender, ConversationStateChangedEventArgs e)
+        {
+            //show the current conversation state
+            this.conversationStatus.Text = e.NewState.ToString();
+        }
+
+        private void AvModalityModalityStateChanged(object sender, ModalityStateChangedEventArgs e)
+        {
+            //updates the status bar with the video channel state
+            this.modalityStatus.Text = e.NewState.ToString();
+        }
+
+        private void AvModalityActionAvailabilityChanged(object sender, ModalityActionAvailabilityChangedEventArgs e)
+        {
+            this.SetModalityStatus(e.Action.ToString());
+
+            // TODO: take some actions depending ot the modality action
+            switch (e.Action)
+            {
+                case ModalityAction.Connect:
+                    break;
+                case ModalityAction.Disconnect:
+                    break;
+                case ModalityAction.Hold:
+                    break;
+                case ModalityAction.Retrieve:
+                    break;
+                case ModalityAction.LocalTransfer:
+                    break;
+                case ModalityAction.ConsultAndTransfer:
+                    break;
+                case ModalityAction.Forward:
+                    break;
+                case ModalityAction.Accept:
+                    break;
+                case ModalityAction.Reject:
+                    break;
+            }
+        }
+
+        private void AudioChannelStateChanged(object sender, ChannelStateChangedEventArgs e)
+        {
+            this.SetAudioStatus(e.NewState.ToString());
+        }
+
+        private void VideoChannelStateChanged(object sender, ChannelStateChangedEventArgs e)
+        {
+            this.SetVideoStatus(e.NewState.ToString());
+
+
+            ////*****************************************************************************************
+            ////                              Video Content
+            ////
+            //// The video content is only available when the Lync client is running in UISuppressionMode.
+            ////
+            //// The video content is not directly accessible as a stream. It's rather available through
+            //// a video window that can de drawn in any panel or window.
+            ////
+            //// The outgoing video is accessible from videoChannel.CaptureVideoWindow
+            //// The window will be available when the video channel state is either Send or SendReceive.
+            //// 
+            //// The incoming video is accessible from videoChannel.RenderVideoWindow
+            //// The window will be available when the video channel state is either Receive or SendReceive.
+            ////
+            ////*****************************************************************************************
+
+            ////if the outgoing video is now active, show the video (which is only available in UI Suppression Mode)
+            //if ((e.NewState == ChannelState.Send
+            //    || e.NewState == ChannelState.SendReceive) && videoChannel.CaptureVideoWindow != null)
+            //{
+            //    //presents the video in the panel
+            //    ShowVideo(panelOutgoingVideo, videoChannel.CaptureVideoWindow);
+            //}
+
+            ////if the incoming video is now active, show the video (which is only available in UI Suppression Mode)
+            //if ((e.NewState == ChannelState.Receive
+            //    || e.NewState == ChannelState.SendReceive) && videoChannel.RenderVideoWindow != null)
+            //{
+            //    //presents the video in the panel
+            //    ShowVideo(panelIncomingVideo, videoChannel.RenderVideoWindow);
+            //}
+        }
+
+        ///// <summary>
+        ///// Shows the specified video window in the specified panel.
+        ///// </summary>
+        //private static void ShowVideo(Panel videoPanel, VideoWindow videoWindow)
+        //{
+        //    //Win32 constants:                  WS_CHILD | WS_CLIPCHILDREN | WS_CLIPSIBLINGS;
+        //    const long lEnableWindowStyles = 0x40000000L | 0x02000000L | 0x04000000L;
+        //    //Win32 constants:                   WS_POPUP| WS_CAPTION | WS_SIZEBOX
+        //    const long lDisableWindowStyles = 0x80000000 | 0x00C00000 | 0x00040000L;
+        //    const int OATRUE = -1;
+
+        //    try
+        //    {
+        //        //sets the properties required for the native video window to draw itself
+        //        videoWindow.Owner = videoPanel.Handle.ToInt32();
+        //        videoWindow.SetWindowPosition(0, 0, videoPanel.Width, videoPanel.Height);
+
+        //        //gets the current window style to modify it
+        //        long currentStyle = videoWindow.WindowStyle;
+
+        //        //disables borders, sizebox, close button
+        //        currentStyle = currentStyle & ~lDisableWindowStyles;
+
+        //        //enables styles for a child window
+        //        currentStyle = currentStyle | lEnableWindowStyles;
+
+        //        //updates the current window style
+        //        videoWindow.WindowStyle = (int)currentStyle;
+
+        //        //updates the visibility
+        //        videoWindow.Visible = OATRUE;
+        //    }
+        //    catch (Exception exception)
+        //    {
+        //        Console.WriteLine(exception);
+        //    }
+        //}
     }
 }
